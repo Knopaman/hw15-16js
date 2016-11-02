a950818752f92c7f8151fda31c72b25434de17d3
Knopaman <antikor223@gmail.com>
2016-11-02 19:36:40 +0300

    zdarova

--- js/15-16.js
+++ js/15-16.js
@@ -1,19 +1,29 @@
 $(function(){
-$("#btn").click(function(){
-    var txt = $("#search").val();
-    console.log(txt);
-    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+txt+"&tagmode=any&format=json&jsoncallback=?",
-        function(data){
-            console.log(data);
-            $.each(data.items, function(i,item){
-                var div =$("<div/>").appendTo("#result");
-                $("<img/>").attr("src", item.media.m).appendTo(div);
-                if ( i == 10 ) return false;
+
+    function search(){
+        var txt = $("#search").val();
+        console.log(txt);
+        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+txt+"&tagmode=any&format=json&jsoncallback=?",
+            function(data){
+                console.log(data);
+                $.each(data.items, function(i,item){
+                    var div =$("<div/>").appendTo("#result");
+                    $("<img/>").attr("src", item.media.m).appendTo(div);
+                    if ( i == 10 ) return false;
+                });
             });
+    }
+
+$("#btn").click(function(){
+    search()
         });
+
+
+
+$("#search").keypress(function(e){
+    if(e.keyCode == 13){
+        search()
+    }
 });
 
 });
-
-
-
